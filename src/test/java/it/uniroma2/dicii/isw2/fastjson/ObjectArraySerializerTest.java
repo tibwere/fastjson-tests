38ca772d92b9922e1200345cfe117ca57fa66d58
Simone Tiberi <tib.sim.98@gmail.com>
2022-05-06 11:19:14 +0200

    Refactoring of ObjectArraySerializerTest parametrized test cases

--- src/test/java/it/uniroma2/dicii/isw2/fastjson/ObjectArraySerializerTest.java
+++ src/test/java/it/uniroma2/dicii/isw2/fastjson/ObjectArraySerializerTest.java
@@ -3,6 +3,7 @@ package it.uniroma2.dicii.isw2.fastjson;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.JSONSerializer;
 import com.alibaba.fastjson.serializer.SerializeWriter;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,67 +21,62 @@ import static org.junit.Assert.assertEquals;
 public class ObjectArraySerializerTest {
 
     @RunWith(value = Parameterized.class)
-    public static class Type012ObjectArraySerializerTest {
-
-        private int initialSize;
-        private Object [] objects;
+    public static class ObjectArraySerializerTest012Tests {
+        private SerializeWriter out;
+        private Object[] objects;
         private String expected;
 
-        public Type012ObjectArraySerializerTest(int initialSize, Object[] objects, String expected) {
+        public ObjectArraySerializerTest012Tests(int initialSize, Object[] objects, String expected) {
             configure(initialSize, objects, expected);
         }
 
-        private void configure(int initialSize, Object[] objects, String expected) {
-            this.initialSize = initialSize;
-            this.objects = objects;
+        public void configure(int initialSize, Object[] objects, String expected) {
+            this.out = new SerializeWriter(initialSize);
             this.expected = expected;
+            JSONSerializer.write(this.out, objects);
         }
 
         @Parameterized.Parameters
         public static Collection<Object[]> testCasesTuples() {
             return Arrays.asList(new Object[][] {
-                    {1, new Object[] {"a12", "b34"}, "[\"a12\",\"b34\"]"},
+                    {1, new Object[] { "a12", "b34" }, "[\"a12\",\"b34\"]"},
                     {1, new Object[] {}, "[]"},
-                    {1,new Object[] { null, null }, "[null,null]"},
+                    {1, new Object[] {null, null}, "[null,null]"}
             });
         }
 
         @Test
         public void test012() {
-            SerializeWriter out = new SerializeWriter(this.initialSize);
-            JSONSerializer.write(out, this.objects);
-            assertEquals(this.expected, out.toString());
+            assertEquals(this.expected, this.out.toString());
         }
     }
 
     @RunWith(value = Parameterized.class)
-    public static class Type3ObjectArraySerializerTest {
-
-        private Object [] objects;
-        private boolean isPrettyFormat;
+    public static class ObjectArraySerializerTest3Tests {
+        private boolean prettyFormat;
+        private Object[] objects;
         private String expected;
 
-        public Type3ObjectArraySerializerTest(Object[] objects, boolean isPrettyFormat, String expected) {
-            configure(objects, isPrettyFormat, expected);
+        public ObjectArraySerializerTest3Tests(boolean prettyFormat, Object[] objects, String expected) {
+            configure(prettyFormat, objects, expected);
         }
 
-        private void configure(Object []objects, boolean isPrettyFormat, String expected) {
+        public void configure(boolean prettyFormat, Object[] objects, String expected) {
+            this.prettyFormat = prettyFormat;
             this.objects = objects;
-            this.isPrettyFormat = isPrettyFormat;
             this.expected = expected;
         }
 
         @Parameterized.Parameters
         public static Collection<Object[]> testCasesTuples() {
             return Arrays.asList(new Object[][] {
-                    {new Object[] { null, null }, false, "[null,null]"}
+                    {false, new Object[] {null, null}, "[null,null]"}
             });
         }
 
         @Test
         public void test3() {
-            assertEquals(this.expected, JSON.toJSONString(this.objects, this.isPrettyFormat));
+            assertEquals(this.expected, JSON.toJSONString(this.objects, this.prettyFormat));
         }
     }
-
 }
