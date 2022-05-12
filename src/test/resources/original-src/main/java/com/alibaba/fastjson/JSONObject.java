e8890ab61bd8e9dd113173f8f7956e7cf70da254
Simone Tiberi <tib.sim.98@gmail.com>
2022-05-12 15:37:34 +0200

    Changed original source files according to the correct version (1.2.79)

--- src/test/resources/original-src/main/java/com/alibaba/fastjson/JSONObject.java
+++ src/test/resources/original-src/main/java/com/alibaba/fastjson/JSONObject.java
@@ -122,11 +122,6 @@ public class JSONObject extends JSON implements Map<String, Object>, Cloneable, 
         return val;
     }
 
-    public Object getOrDefault(Object key, Object defaultValue) {
-        Object v;
-        return ((v = get(key)) != null) ? v : defaultValue;
-    }
-
     public JSONObject getJSONObject(String key) {
         Object value = map.get(key);
 
--- src/test/resources/original-src/main/java/com/alibaba/fastjson/parser/deserializer/ArrayListTypeFieldDeserializer.java
+++ src/test/resources/original-src/main/java/com/alibaba/fastjson/parser/deserializer/ArrayListTypeFieldDeserializer.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.alibaba.fastjson.parser.DefaultJSONParser;
 import com.alibaba.fastjson.parser.Feature;
@@ -18,7 +17,6 @@ import com.alibaba.fastjson.parser.ParseContext;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.util.FieldInfo;
 import com.alibaba.fastjson.util.ParameterizedTypeImpl;
-import jdk.nashorn.internal.parser.JSONParser;
 
 public class ArrayListTypeFieldDeserializer extends FieldDeserializer {
 
@@ -171,7 +169,7 @@ public class ArrayListTypeFieldDeserializer extends FieldDeserializer {
 
             lexer.nextToken(itemFastMatchToken);
 
-            for (int i = 0; ; ++i) {
+            for (int i = 0;; ++i) {
                 if (lexer.isEnabled(Feature.AllowArbitraryCommas)) {
                     while (lexer.token() == JSONToken.COMMA) {
                         lexer.nextToken();
@@ -195,11 +193,6 @@ public class ArrayListTypeFieldDeserializer extends FieldDeserializer {
             }
 
             lexer.nextToken(JSONToken.COMMA);
-        } else if (token == JSONToken.LITERAL_STRING && fieldInfo.unwrapped) {
-            String str = lexer.stringVal();
-            lexer.nextToken();
-            DefaultJSONParser valueParser = new DefaultJSONParser(str);
-            valueParser.parseArray(array);
         } else {
             if (itemTypeDeser == null) {
                 itemTypeDeser = deserializer = parser.getConfig().getDeserializer(itemType);
